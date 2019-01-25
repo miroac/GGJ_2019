5a7a476385ebf3a39705f216ad3994096e2ad301
miroac <michael.kerr.graham@gmail.com>
2019-01-25 20:17:30 +1100

    Default Gameplay Classes

---
+++ Poltergeist/Source/Poltergeist/Poltergeist.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Poltergeist.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Poltergeist, "Poltergeist" );
